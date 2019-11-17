5c9c8badedead083dfb3a309ee63cddc0fcbc84c
customerservice <customerservice@elitenet.dk>
2019-11-17 15:03:38 +0100

    json

--- jqueryproductselector/README.md
+++ jqueryproductselector/README.md
@@ -1,2 +1,5 @@
 Responsive product selector based on javascript/jquery 1.7.2.
-Also called filtered search. Option boxes change based on previous selection. 
+Also called filtered search. Option boxes change based on previous selection.
+Code reads json files or could be used with API webservices.
+
+See live at https://docs.elitenet.dk/javascriptproductselector/
--- jqueryproductselector/productselector.js
+++ jqueryproductselector/productselector.js
@@ -1,33 +1,70 @@
-﻿$(document).ready(function () {
- $('*[id*=item]').each(function () {
-  $(this).prop('selectedIndex', 0);
+﻿(function () {
+
+ $(document).ready(function () {
+  // SET AJAXFORMAT
+  $.ajaxSetup({
+   scriptCharset: "utf-8",
+   contentType: "application/json; charset=utf-8"
+  });
+
+  // SERVICE FUNCTIONS
+  function RemoveAllStartOver(param) {
+   $(param + ' option').not('[data-headline]').remove();
+  }
+
+  // SELECT HEADLINE ON RELOAD
+  $('*[id*=item]').each(function () {
+   $(this).prop('selectedIndex', 0);
+  });
+  // RESET DISABLED
+  $('#select-2,#select-3').prop("disabled", true);
+
+  //POPULATE FIRST
+  $.getJSON("json/_Brands.json", function (data) {
+   $.each(data, function (key, val) {
+    var o = new Option(val, key);
+    $(o).html(val);
+    $("#select-1").append(o);
+   });
+  });
+
+  // CHANGE SELECT-1
+  $('#select-1').change(function () {
+   //REMOVE ALL START OVER
+   RemoveAllStartOver("#select-2");
+   RemoveAllStartOver("#select-3");
+   $('#select-3').prop("disabled", true);
+
+
+   $.getJSON("json/" + $(this).val() + ".json", function (data) {
+    $.each(data, function (key, val) {
+     var o = new Option(val, key);
+     $(o).html(val);
+     $("#select-2").append(o);
+    });
+    $('#select-2').prop("disabled", false);
+   });
+  });
+
+  // CHANGE SELECT-2
+  $('#select-2').change(function () {
+   //REMOVE ALL START OVER
+   RemoveAllStartOver("#select-3");
+
+   $.getJSON("json/" + $(this).val() + ".json", function (data) {
+    $.each(data, function (val, key) {
+     var o = new Option(val, key);
+     $(o).html(val);
+     $("#select-3").append(o);
+    });
+    $('#select-3').prop("disabled", false);
+   });
+  });
+
+  // EXE URL SELECT-3
+  $('#select-3').change(function () {
+   window.location = $(this).val();
+  });
  });
 
- hideOptions("#item-2");
- hideOptions("#item-3");
-
- $('#item-1').change(function () {
-  hideOptions("#item-2");
-  zeroSelected("#item-2");
-  $('option[data-parent-item="' + $(this).val() + '"]').removeAttr('hidden');
- });
-
- $('#item-2').change(function () {
-  hideOptions("#item-3");
-  zeroSelected("#item-3");
-  $('option[data-parent-itemsub="' + $(this).val() + '"]').removeAttr('hidden');
- });
-
- $('#item-3').change(function () {
-  window.location = $(this).val();
- });
-
- function hideOptions(param) {
-  $(param).find("option").attr('hidden', 'hidden');
- }
-
- function zeroSelected(param) {
-  $(param).prop('selectedIndex', 0);
- }
-
-});
+}());
